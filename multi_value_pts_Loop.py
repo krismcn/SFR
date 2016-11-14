b05da32996bd156942d1c9a9dcc3bfa51f07eeb8
Kris McNyset <kristina.mcnyset@noaa.gov>
2016-11-14 11:28:28 -0800

    This is my standard updated at every iteration to include the current processing file paths.

--- multi_value_pts_Loop.py
+++ multi_value_pts_Loop.py
@@ -17,9 +17,9 @@ gp = arcgisscripting.create()
 # Load required toolboxes...
 gp.AddToolbox("C:\\Program Files (x86)\\ArcGIS\\Desktop10.2\\ArcToolbox\\Toolboxes\\Spatial Analyst Tools.tbx")
 
-gp.workspace = "D:\\OneDrive\\work\\GIS\\8_day_1km_LST\\LST_s1_2013\\"
-outfolder = "D:\\OneDrive\\work\\GIS\\8_day_1km_LST\\LST_s1_2013\\"
-inPointFeatures = "D:\\OneDrive\\work\\GIS\\8_day_1km_LST\\LST_s1_2013\\JD_13_LST.shp"
+gp.workspace = "D:\\OneDrive\\work\\research\\CHaMP\\GIS\\LST\\LST_s1_2013\\"
+outfolder = "D:\\OneDrive\\work\\research\\CHaMP\\GIS\\LST\\LST_s1_2013\\"
+inPointFeatures = "D:\\OneDrive\\work\\research\\CHaMP\\GIS\\LST\\LST_s1_2013\\YF_13_LST.shp"
 
 readFieldList = open("D:\\OneDrive\\work\\GIS\\8_day_1km_LST\\LST_s1_2013\\gridlist_num.txt", 'r')
 FieldList = readFieldList.readlines()
@@ -28,7 +28,7 @@ readFieldList.close
 for item in FieldList:
     item = item.replace("\n","")
     print item
-    inraster = "lsts1" + item 
+    inraster = "lst_s1" + item 
     arcpy.gp.ExtractMultiValuesToPoints_sa(inPointFeatures, [[inraster, item]], "NONE")  
     
     
